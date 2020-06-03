eb1d40fe31329aae152858e90cb11e5bb4213982
Konstanty Białek <tykfa90@gmail.com>
2020-06-03 11:59:50 +0200

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Numerics;
+
+namespace zadanie_zaliczeniowe_konstanty_bialek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program.Silnia(4);
+        }
+
+        private static BigInteger Silnia(int liczba)
+        {
+            if (liczba == 0)
+                return 1;
+            else
+                return liczba * Silnia(liczba - 1);
+        }
+    }
+}
